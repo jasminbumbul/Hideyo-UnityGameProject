228bb25d7640858ea3e29c48d77700f207e37cf4
numko99 <61965732+numko99@users.noreply.github.com>
2020-04-17 23:07:21 +0200

    enemy_tenk,promjena_kamere_nista_posebno

---
+++ New Unity Project/Assets/Scripts/Enemy_atack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_atack : MonoBehaviour
+{
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        Debug.Log(direction);
+    }
+}
