788e3ece70ad77e144d1c751d2d15d2b57fdf67c
Jasmin Bumbul <jasmin.bumbul@edu.fit.ba>
2020-05-05 11:11:00 +0200

    dodan  player,njegove animacije i nova skripta za kretanje

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float ForwardSpeed = 10f;
+    public float BackwardSpeed = 3f;
+    public float SideSpeed = 3f;
+
+    private CharacterController CharacterController;
+    public float JumpHeight = 3f;
+
+    private  float Gravity = -9.81f;
+    Vector3 Velocity;
+    public Transform GroundCheck;
+    public float GroundRadius = 0.04f;
+    public LayerMask GroundMask;
+    private bool IsGrounded;
+
+    private Animator Animator;
+
+    private void Start()
+    {
+        CharacterController = GetComponent<CharacterController>();
+        Animator = GameObject.Find("HumanModel").GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        IsGrounded = Physics.CheckSphere(GroundCheck.position, GroundRadius, GroundMask);
+
+        if (IsGrounded && Velocity.y<0)
+        {
+            Velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 Movement = transform.right * x + transform.forward * z;
+
+        //razlicite brzine za razlicite smjerove kretanja
+        if (z > 0)
+        {
+            CharacterController.Move(Movement * ForwardSpeed * Time.deltaTime);
+            Animator.SetBool("IsWalking", true);
+        }
+        if (z<0)
+        {
+            CharacterController.Move(Movement * BackwardSpeed * Time.deltaTime);
+            Animator.SetBool("IsWalking", true);
+        }
+        if (z==0 && (x<0 || x>0))
+        {
+            CharacterController.Move(Movement * SideSpeed * Time.deltaTime);
+            Animator.SetBool("IsWalking", true);
+        }
+        if (x==0 && z==0)
+        {
+            Animator.SetBool("IsWalking", false);
+        }
+
+        //brzo trcanje na leftShift
+        if (Input.GetAxis("FasterWalking")>0 && Animator.GetBool("IsWalking"))
+        {
+            Animator.SetBool("IsRunning", true);
+            ForwardSpeed =15f;
+        }
+        else 
+        {
+            Animator.SetBool("IsRunning", false);
+            ForwardSpeed = 10f;
+        }
+
+        //skok
+        if (Input.GetButton("Jump") && IsGrounded)
+        {
+            Velocity.y = Mathf.Sqrt(JumpHeight*-2f * Gravity);
+            Animator.SetBool("IsJumping", true);
+        }
+        else
+        {
+            Animator.SetBool("IsJumping", false);
+        }
+
+        //primjena gravitacije
+        Velocity.y += Gravity * Time.deltaTime;
+        Velocity.y += Gravity * Time.deltaTime;
+        CharacterController.Move(Velocity* Time.deltaTime);
+
+        //punch
+        if (Input.GetMouseButton(0))
+        {
+            Animator.SetBool("IsPunching", true);
+        }
+        else
+        {
+            Animator.SetBool("IsPunching", false);
+        }
+    }
+}
