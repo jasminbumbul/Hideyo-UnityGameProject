53dac51c182c9c2eb15345b2e852eddf9ea87f64
numko99 <61965732+numko99@users.noreply.github.com>
2020-05-07 15:27:20 +0200

    enemy_path_fiding

---
+++ New Unity Project/Assets/Scripts/tweg.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class tweg : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    NavMeshAgent agent;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = this.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+        if(agent.remainingDistance<=1)
+        {
+            Debug.Log("fight");
+        }
+    }
+}
